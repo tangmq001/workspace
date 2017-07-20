a17dd831906b8f7e770c1d7a84e2ee08e07000b6
tangmq <2427077090@qq.com>
2017-07-20 11:52:02 +0800

    add more

---
+++ DemoDemo/src/main/java/tang/ming/qiao/service/ILogService.java
@@ -0,0 +1,10 @@
+package tang.ming.qiao.service;
+
+/**
+ * @Author:tangmq
+ * @Date:2017/7/19
+ * @Note:
+ */
+public interface ILogService {
+
+}
---
+++ DemoDemo/src/main/java/tang/ming/qiao/service/impl/LogServiceImpl.java
@@ -0,0 +1,11 @@
+package tang.ming.qiao.service.impl;
+
+import tang.ming.qiao.service.ILogService;
+
+/**
+ * @Author:tangmq
+ * @Date:2017/7/19
+ * @Note:
+ */
+public class LogServiceImpl implements ILogService {
+}
